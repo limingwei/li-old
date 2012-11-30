e69e74004853d4ab512af2fef7ddec68c2b3caad
黎明伟 <416133823@qq.com>
2012-12-01 00:17:02 +0800

    添加行fastjson测试

--- test/demo/T.java
+++ test/demo/T.java
@@ -3,6 +3,8 @@ package demo;
 import li.dao._User;
 import li.ioc.Ioc;
 
+import com.alibaba.fastjson.JSON;
+
 public class T {
     public static void main(String[] args) throws Exception {
         _User userDao = Ioc.get(_User.class);
@@ -11,7 +13,8 @@ public class T {
         System.out.println(userDao.list(null, "select * from mysql.db"));
         System.out.println(userDao.list(null, "select version()"));
         System.out.println(userDao.list(null, "select database()"));
-
         System.out.println(userDao.list(null, "select * from discuz_x25_sc_utf8.dx25su_forum_post"));
+
+        System.out.println(JSON.toJSONString(userDao.list(null)));
     }
 }
