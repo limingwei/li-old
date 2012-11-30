9aebfb09979ffff27692640b221d3a3e3bd1dc33
黎明伟 <416133823@qq.com>
2012-11-30 22:33:55 +0800

    druid相关更新

--- more/li/dao/DruidAdapter.java
+++ more/li/dao/DruidAdapter.java
@@ -2,6 +2,7 @@ package li.dao;
 
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import li.util.Files;
@@ -17,11 +18,7 @@ public class DruidAdapter extends DruidDataSource {
      * 重写setFilters方法
      */
     public void setFilters(String filters) throws SQLException {
-        if (filters == null || filters.length() == 0) {
-            return;
-        }
-        String[] filterArray = filters.split("\\,");
-
+        String[] filterArray = (filters != null && filters.length() > 0) ? filters.split("\\,") : new String[] {};
         for (String item : filterArray) {
             if ("wall".equals(item)) {
                 loadWallFilter();
@@ -36,32 +33,24 @@ public class DruidAdapter extends DruidDataSource {
      */
     private void loadWallFilter() {
         Properties properties = Files.load("config.properties");
-
-        // 禁止使用的func列表
-        String[] PERMIT_FUNCTIONS = properties.getProperty("DRUID.PERMIT_FUNCTIONS").split(",");
-        // 禁止访问的数据库列表
-        String[] PERMIT_SCHEMAS = properties.getProperty("DRUID.PERMIT_SCHEMAS").split(",");
-        // 禁止访问的table列表
-        String[] PERMIT_TABLES = properties.getProperty("DRUID.PERMIT_TABLES").split(",");
-        // 只读table列表
-        String[] READONLY_TABLES = properties.getProperty("DRUID.READONLY_TABLES").split(",");
-        // 禁止访问的系统对象列表
-        String[] PERMIT_OBJECTS = properties.getProperty("DRUID.PERMIT_OBJECTS").split(",");
-        //
-        String[] PERMIT_VARIANTS = properties.getProperty("DRUID.PERMIT_VARIANTS").split(",");
-
         WallConfig wallConfig = new WallConfig();
-
-        wallConfig.getPermitFunctions().addAll(Arrays.asList(PERMIT_FUNCTIONS));
-        wallConfig.getPermitObjects().addAll(Arrays.asList(PERMIT_OBJECTS));
-        wallConfig.getPermitSchemas().addAll(Arrays.asList(PERMIT_SCHEMAS));
-        wallConfig.getPermitTables().addAll(Arrays.asList(PERMIT_TABLES));
-        wallConfig.getPermitVariants().addAll(Arrays.asList(PERMIT_VARIANTS));
-        wallConfig.getReadOnlyTables().addAll(Arrays.asList(READONLY_TABLES));
-
         WallFilter wallFilter = new WallFilter();
-        wallFilter.setConfig(wallConfig);
 
+        wallConfig.getPermitFunctions().addAll(getProps(properties, "DRUID.PERMIT_FUNCTIONS"));// 禁止使用的func列表
+        wallConfig.getPermitObjects().addAll(getProps(properties, "DRUID.PERMIT_OBJECTS"));// 禁止访问的系统对象列表
+        wallConfig.getPermitSchemas().addAll(getProps(properties, "DRUID.PERMIT_SCHEMAS"));// 禁止访问的数据库列表
+        wallConfig.getPermitTables().addAll(getProps(properties, "DRUID.PERMIT_TABLES"));// 禁止访问的table列表
+        wallConfig.getReadOnlyTables().addAll(getProps(properties, "DRUID.READONLY_TABLES"));// 只读table列表
+        wallConfig.getPermitVariants().addAll(getProps(properties, "DRUID.PERMIT_VARIANTS"));
+
+        wallFilter.setConfig(wallConfig);
         super.getProxyFilters().add(wallFilter);
+    }
+
+    /*
+     * 从Properties里加载配置
+     */
+    private List<String> getProps(Properties properties, String key) {
+        return Arrays.asList(properties.getProperty(key).split(","));
     }
 }
