dd2cf0ec982d872e4cfc9374701a2eb232ff07bc
黎明伟 <416133823@qq.com>
2012-12-02 22:49:17 +0800

    调整测试

--- test/demo/T.java
+++ test/demo/T.java
@@ -9,11 +9,11 @@ public class T {
     public static void main(String[] args) throws Exception {
         _User userDao = Ioc.get(_User.class);
 
-        // System.out.println(userDao.list(null, "select * from framework_forum.t_account"));
-        // System.out.println(userDao.list(null, "select * from mysql.db"));
-        // System.out.println(userDao.list(null, "select version()"));
-        // System.out.println(userDao.list(null, "select database()"));
-        // System.out.println(userDao.list(null, "select * from discuz_x25_sc_utf8.dx25su_forum_post"));
+        System.out.println(userDao.list(null, "select * from framework_forum.t_account"));
+        System.out.println(userDao.list(null, "select * from mysql.db"));
+        System.out.println(userDao.list(null, "select version()"));
+        System.out.println(userDao.list(null, "select database()"));
+        System.out.println(userDao.list(null, "select * from discuz_x25_sc_utf8.dx25su_forum_post"));
 
         System.out.println(JSON.toJSONString(userDao.list(null)));
 
