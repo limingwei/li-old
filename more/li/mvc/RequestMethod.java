bb0cc9fc328c5c8e7c634d0b6063f25f4a7893b1
li <limw@w.cn>
2012-12-11 13:06:36 +0800

    抽出接口RequestMethod

---
+++ more/li/mvc/RequestMethod.java
@@ -0,0 +1,19 @@
+package li.mvc;
+
+public interface RequestMethod {
+    public static final String GET = "GET";
+
+    public static final String POST = "POST";
+
+    public static final String PUT = "PUT";
+
+    public static final String DELETE = "DELETE";
+
+    public static final String HEAD = "HEAD";
+
+    public static final String OPTIONS = "OPTIONS";
+
+    public static final String TRACE = "TRACE";
+
+    public static final String ALL = ".*";// 任何类型
+}
