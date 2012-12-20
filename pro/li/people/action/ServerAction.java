29345c0e2e726b9b585aaf1323dd2b44f2fe2ee6
黎明伟 <416133823@qq.com>
2012-12-20 22:26:30 +0800

    pro

--- pro/li/people/action/ServerAction.java
+++ pro/li/people/action/ServerAction.java
@@ -9,10 +9,10 @@ import li.people.Const;
 import li.people.record.Server;
 
 @Bean
-public class ServerAction extends AbstractAction implements Const{
+public class ServerAction extends AbstractAction implements Const {
     @Inject
     Server serverDao;
-    
+
     @At("server_list.do")
     public void list(Page page) {
         setRequest(LIST, serverDao.list(page));
