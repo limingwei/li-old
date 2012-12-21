3d3c1e07e42ebf9493dbab046d8df10302172876
li <limw@w.cn>
2012-12-21 17:50:52 +0800

    Mail整理

--- dev/li/mail/Demo.java
+++ dev/li/mail/Demo.java
@@ -9,19 +9,20 @@ import java.util.Map;
  * 
  * http://sendgrid.com
  */
-public class JavaMail {
+public class Demo {
     public static void main(String[] args) {
         Sender sender = new Sender("smtp.mailgun.org", "postmaster@limingwei.mailgun.org", "6mitwv670n61");
 
         Mail mail = new Mail();
         mail.setSubject("这是一封测试邮件" + System.currentTimeMillis());
         mail.setContent(buildContent());
+        mail.setFrom("li@w.cn");//发件人
         mail.setTo("416133823@qq.com");
         sender.send(mail);
     }
 
     private static String buildContent() {
-        Beetl beetl = new Beetl(new File("E:\\workspace\\li\\dev\\li\\mail\\mail_1.htm"));
+        Beetl beetl = new Beetl(new File(System.getProperty("user.dir") + "\\dev\\li\\mail\\mail_1.htm"));
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("name", "黎明伟");
         map.put("date", "2012-10-22");
