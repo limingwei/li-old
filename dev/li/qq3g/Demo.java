e8419989437c2fd2d6f63e560edfad44cd7d0bc0
黎明伟 <416133823@qq.com>
2012-12-29 00:40:27 +0800

    qq3g更新

--- dev/li/qq3g/Demo.java
+++ dev/li/qq3g/Demo.java
@@ -10,12 +10,22 @@ public class Demo {
         // System.out.println(qq3g.refresh(sid, "416133823"));
         // System.out.println(qq3g.send(sid, "你好，这是一条测试消息，来自 黎明伟-3GQQ-JAVA", "416133823"));
 
-        send("416133823", "616319864", "154520174", "767702147");
+        // send("416133823", "616319864", "154520174", "767702147");
+
+        sendMsgsToQQ("416133823", new String[] { "床前明月光", "疑是地上霜", "举头望明月", "低头思故乡" });
     }
 
-    public static void send(String... qqs) throws Exception {
+    public static void sendMsgToQQs(String msg, String[] qqs) throws Exception {
         for (String qq : qqs) {
-            System.out.println(qq3g.send(sid, "你好,这是一条测试消息,收到请回答,来自 黎明伟-3GQQ-JAVA", qq));
+            // System.out.println(qq3g.send(sid, "你好,这是一条测试消息,收到请回答,来自 黎明伟-3GQQ-JAVA", qq));
+            System.out.println(qq3g.send(sid, msg, qq));
+            Thread.sleep(1000);
+        }
+    }
+
+    public static void sendMsgsToQQ(String qq, String[] msgs) throws Exception {
+        for (String msg : msgs) {
+            System.out.println(qq3g.send(sid, msg, qq));
             Thread.sleep(1000);
         }
     }
