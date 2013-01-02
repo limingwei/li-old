11f6c8ec118e5bdf367176260466ffbbcf291ac5
黎明伟 <416133823@qq.com>
2013-01-02 12:39:43 +0800

    更新Dao多线程测试

--- test/demo/MultiThreadDao.java
+++ test/demo/MultiThreadDao.java
@@ -5,17 +5,17 @@ import li.ioc.Ioc;
 
 public class MultiThreadDao {
     public static void main(String[] args) throws Exception {
-        final _Account account = Ioc.get(_Account.class);
+        final _Account accountDao = Ioc.get(_Account.class);
 
         for (int i = 0; i < 1000; i++) {// 开启1000个线程
             new Thread() {
                 public void run() {
                     for (; true;) {// 每个线程无限循环
-                        account.update("SET password=password");// 这是一个带Aop带Trans的Dao方法
+                        accountDao.update("SET status=1 WHERE id=11");// 这是一个带Aop带Trans的Dao方法
                     }
                 }
             }.start();
-            Thread.sleep(100);// 每隔0.1秒增加一个线程
+            Thread.sleep(1000);// 每隔1秒增加一个线程
         }
     }
 }
