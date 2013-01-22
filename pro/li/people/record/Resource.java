b90783375ccf46797a8e302af7b258cc9300a0f8
li <limw@w.cn>
2013-01-22 13:58:10 +0800

    pro更新

--- pro/li/people/record/Resource.java
+++ pro/li/people/record/Resource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import li.annotation.Bean;
 import li.annotation.Table;
+import li.dao.Page;
 import li.dao.Record;
 import li.people.Const;
 
@@ -13,6 +14,10 @@ public class Resource extends Record<Resource> implements Const {
     private static final long serialVersionUID = 7989017780707672816L;
 
     public List<Resource> listByRoleId(Integer roleId) {
-        return list(MAX_PAGE, "WHERE id IN (SELECT resource_id from r_role_resource WHERE role_id=?)", roleId);
+        return list(MAX_PAGE, "WHERE id IN (SELECT resource_id from r_role_resource WHERE role_id=?) ORDER BY description ASC", roleId);
+    }
+
+    public List<Resource> list(Page page) {
+        return super.list(page, "SELECT * FROM t_resource ORDER BY description ASC");
     }
 }
