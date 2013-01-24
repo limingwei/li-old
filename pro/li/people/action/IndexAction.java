19168c0eb5a1bd87030cba18e4a0fe459d18e363
li <limw@w.cn>
2013-01-24 11:44:55 +0800

    pro更新

--- pro/li/people/action/IndexAction.java
+++ pro/li/people/action/IndexAction.java
@@ -9,6 +9,10 @@ public class IndexAction extends AbstractAction {
 
     @At("index.do")
     public void index() {
-        view("index");
+        if (null == getSession("account")) {
+            redirect("login.do");
+        } else {
+            view("index");
+        }
     }
 }
