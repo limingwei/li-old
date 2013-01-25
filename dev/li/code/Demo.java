ec71a816cb1d1c9db37373a97d43f364c57ff67b
li <limw@w.cn>
2013-01-25 15:39:39 +0800

    验证码

--- dev/li/code/Demo.java
+++ dev/li/code/Demo.java
@@ -6,24 +6,41 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 /**
- * http://nvcsz.gtimg.com/123123/7383631461139009302.gif?r=15022
+ * http://nvcsz.gtimg.com/123123/7383631461139009302.gif?r=15022 tesseract
  */
 public class Demo {
     public static void main(String[] args) throws Exception {
         File file = new File(System.getProperty("user.dir") + "/dev/li/code/1.gif");
-        BufferedImage bufferedImage = ImageIO.read(file);
-        int width = bufferedImage.getWidth();
-        int height = bufferedImage.getHeight();
+        BufferedImage in = ImageIO.read(file);
+        int width = in.getWidth();
+        int height = in.getHeight();
+
+        BufferedImage out = new BufferedImage(width, height, in.getType());
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                int rgb = bufferedImage.getRGB(x, y);
-                int r = (int) (((rgb >> 16) & 0xFF) * 1.1 + 30);
-                int g = (int) (((rgb >> 8) & 0xFF) * 1.1 + 30);
-                int b = (int) (((rgb >> 0) & 0xFF) * 1.1 + 30);
+                int rgb = in.getRGB(x, y);
 
-                System.out.println(r + "\t" + g + "\t" + b);
+                out.setRGB(x, y, 0x00_FF_00_00);
             }
         }
+
+        ImageIO.write(out, "PNG", new File("C:/Users/li/Desktop/out.png"));
+    }
+
+    public static int getAlpha(int pixel) {
+        return (pixel & 0xFF000000) >> 24;
+    }
+
+    public static int getRed(int pixel) {
+        return (pixel & 0x00FF0000) >> 16;
+    }
+
+    public static int getGreen(int pixel) {
+        return (pixel & 0x0000FF00) >> 8;
+    }
+
+    public static int getBlue(int pixel) {
+        return (pixel & 0x000000FF);
     }
 }
