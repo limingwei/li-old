939da4671110fc5c78793488b785dd0ded0156c8
黎明伟 <416133823@qq.com>
2013-01-26 11:16:46 +0800

    pro-vali 更新

--- WebContent/source/bootstrap-validation/bootstrap-validation-2.js
+++ WebContent/source/bootstrap-validation/bootstrap-validation-2.js
@@ -1,21 +1,4 @@
-/* =========================================================
- * bootstrap-validation.js 
- * Original Idea: http:/www.newkou.org (Copyright 2012 Stefan Petre)
- * Updated by 不会飞的羊 (https://github.com/FateSheep/Validation-for-Bootstrap)
- * =========================================================
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================= */
+
 !function($) {
     $.fn.validation = function(options) {
         return this.each(function() {
@@ -23,12 +6,14 @@
             validationForm(this);
         });
     };
-    
-    $.fn.validation2 = function(options) {
-        return this.each(function() {
+
+    $.fn.vali = function(options) {
+    	var flag = true;
+        this.each(function() {
             globalOptions = $.extend({}, $.fn.validation.defaults, options);
-            validationForm2(this);
+            flag = valiForm(this);
         });
+        return flag;
     };
     
     $.fn.validation.defaults = {
@@ -45,7 +30,7 @@
 
     var validateField = function(field, valid) { // 验证字段
         var el = $(field), error = false, errorMsg = '';
-        for (i = 0; i < valid.length; i++) {
+        for (var i = 0; i < valid.length; i++) {
             var x = true, flag = valid[i], msg = (el.attr(flag + '-message')==undefined)?null:el.attr(flag + '-message');;
             if (flag.substr(0, 1) == '!') {
                 x = false;
@@ -53,7 +38,7 @@
             }
 
             var rules = globalOptions.validRules;
-            for (j = 0; j < rules.length; j++) {
+            for (var j = 0; j < rules.length; j++) {
                 var rule = rules[j];
                 if (flag == rule.name) {
                     if (rule.validate.call(field, el.val()) == x) {
@@ -116,13 +101,68 @@
                             scrollTo(0, el[0].offsetTop - 50);
                             wFocus = true;
                         }
+
+                        validationError = true;
+                    }
+                }
+            });
+
+            wFocus = false;
+            fieldState = true;
+
+            if (validationError) {
+                formState = false; 
+
+                $('input, textarea').each(function() {
+                    var el = $(this), valid = (el.attr('check-type')==undefined)?null:el.attr('check-type').split(' ');
+                    if (valid != null && valid.length > 0) {
+                        el.focus(function() { // 获取焦点时
+                            var controls = el.parents('.controls'), controlGroup = el.parents('.control-group'), errorEl = controls.children('.help-block, .help-inline');
+                            if (errorEl.length > 0) {
+                                var help = controls.data('help-message');
+                                if (help == undefined) {
+                                    errorEl.remove();
+                                } else {
+                                    errorEl.text(help);
+                                }
+                            }
+                            controlGroup.attr('class','control-group');
+                        });
+
+                        el.blur(function() { // 失去焦点时
+                            validateField(this, valid);
+                        });
+                    }
+                });                
+                return false;
+            }
+            return true;
+        });
+    };
+    
+    var valiForm = function(obj) { // 表单验证方法
+//        $(obj).submit(function() { // 提交时验证
+/*
+    		if (formState) { // 重复提交则返回
+                return false;
+            }
+*/
+            formState = true;
+            var validationError = false;
+            $('input, textarea', $(obj)).each(function () {
+                var el = $(this), valid = (el.attr('check-type')==undefined)?null:el.attr('check-type').split(' ');
+                if (valid != null && valid.length > 0) {
+                    if (!validateField(this, valid)) {
+                        if (wFocus == false) {
+                            scrollTo(0, el[0].offsetTop - 50);
+                            wFocus = true;
+                        }
                         validationError = true;
                     }
                 }
             });
             wFocus = false;
             fieldState = true;
-
             if (validationError) {
                 formState = false; 
                 $('input, textarea').each(function() {
@@ -147,57 +187,7 @@
                 });                
                 return false;
             }
-
             return true;
-        });
-    };
-
-    var validationForm2 = function(obj) { // 表单验证方法
-        $(obj).find("input, textarea").each(function(){
-        	$(this).blur(function() { // 提交时验证
-                if (formState) { // 重复提交则返回
-                    return false;
-                }
-                formState = true;
-                var validationError = false;
-                    var el = $(this), valid = (el.attr('check-type')==undefined)?null:el.attr('check-type').split(' ');
-                    if (valid != null && valid.length > 0) {
-                        if (!validateField(this, valid)) {
-                            if (wFocus == false) {
-                                scrollTo(0, el[0].offsetTop - 50);
-                                wFocus = true;
-                            }
-                            validationError = true;
-                        }
-                    }
-                wFocus = false;
-                fieldState = true;
-
-                if (validationError) {
-                    formState = false; 
-                        var el = $(this), valid = (el.attr('check-type')==undefined)?null:el.attr('check-type').split(' ');
-                        if (valid != null && valid.length > 0) {
-                            el.focus(function() { // 获取焦点时
-                                var controls = el.parents('.controls'), controlGroup = el.parents('.control-group'), errorEl = controls.children('.help-block, .help-inline');
-                                if (errorEl.length > 0) {
-                                    var help = controls.data('help-message');
-                                    if (help == undefined) {
-                                        errorEl.remove();
-                                    } else {
-                                        errorEl.text(help);
-                                    }
-                                }
-                                controlGroup.attr('class','control-group');
-                            });
-                            el.blur(function() { // 失去焦点时
-                                validateField(this, valid);
-                            });
-                        }
-                    return false;
-                }
-
-                return true;
-            });
-        });
+//        });	//$(obj).submit(function() {
     };
 }(window.jQuery);
