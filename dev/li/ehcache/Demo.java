774a8ea10f9225a3ffc1981de78b804faf8bd039
li <limw@w.cn>
2013-02-05 18:17:35 +0800

    ehcache配置文件及测试

--- dev/li/ehcache/Demo.java
+++ dev/li/ehcache/Demo.java
@@ -5,12 +5,17 @@ import li.ioc.Ioc;
 public class Demo {
     static Hello hello = Ioc.get(Hello.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(hello.say("123"));
         System.out.println(hello.say("123"));
         System.out.println(hello.say("123"));
         System.out.println(hello.say("456"));
         System.out.println(hello.say("456"));
         System.out.println(hello.say("789"));
+
+        for (int i = 0; true; i++) {
+            System.out.println(i + "\t" + hello.say("123"));
+            Thread.sleep(2000);
+        }
     }
 }
