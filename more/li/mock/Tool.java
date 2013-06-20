d5df25740b69f9b230c1f2f9ac798fc963348ed3
li <li@li-pc>
2013-06-20 15:30:16 +0800

    测试代码整理

--- more/li/mock/Tool.java
+++ more/li/mock/Tool.java
@@ -1,17 +1,40 @@
 package li.mock;
 
+/**
+ * MVC MOCK 中用到的 stackTrace 工具
+ * 
+ * @author li (limw@w.cn)
+ * @version 0.1.1 (2013-06-20)
+ */
 public class Tool {
+    /**
+     * 返回当前线程中 stackTrace 列表
+     */
     public static String stackTrace() {
         String string = "";
         StackTraceElement[] elements = Thread.currentThread().getStackTrace();
         for (int i = 0; i < elements.length; i++) {
-            String className = elements[i].getClassName();
-            if (elements[i].getLineNumber() > 0 && !className.startsWith("org.eclipse.jdt.internal.") && !className.startsWith("java.lang.") //
-                    && !className.startsWith("sun.reflect.") && !className.startsWith("org.junit.") && !className.startsWith("net.sf.cglib.proxy.")//
-            /* && !className.startsWith("li.") */) {
-                string += "\n" + (i + "\t" + elements[i].getClassName() + "." + elements[i].getMethodName() + "()\t" + "\t# " + elements[i].getLineNumber());
+            if (!skip(elements[i])) {
+                string += "\n" + (i + "\t#" + elements[i].getLineNumber() + "\t" + elements[i].getClassName() + "." + elements[i].getMethodName() + "()");
             }
         }
         return string;
+    }
+
+    /**
+     * 一个StackTraceElement是否应当跳过
+     */
+    private static Boolean skip(StackTraceElement element) {
+        if (element.getLineNumber() < 1) {
+            return true;
+        }
+        String[] headers = { "org.eclipse.jdt.internal.junit", "java.lang.Thread", "java.lang.reflect.Method", "sun.reflect.", "li.mock.Mock",//
+                "org.junit.runners.", "org.junit.internal.runners.", "net.sf.cglib.proxy.", "li.mock.Tool", "li.aop.AopChain", "li.aop.AopEnhancer" };
+        for (String header : headers) {
+            if (element.getLineNumber() > 0 && element.getClassName().startsWith(header)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- test/li/mvc/ContextTest.java
+++ test/li/mvc/ContextTest.java
@@ -1,6 +1,9 @@
 package li.mvc;
 
 import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
 import li.test.ActionTest;
 
 import org.junit.Before;
@@ -20,5 +23,33 @@ public class ContextTest extends ActionTest {
     @Test
     public void getResponse() {
         assertNotNull(Context.getResponse());
+    }
+
+    @Test
+    public void httl() {
+        Ctx.setRequest("name", "limingwei");
+        Ctx.setRequest("date", new Date());
+        Ctx.httl("WEB-INF/view_ht/httl.httl");
+    }
+
+    @Test
+    public void freemarker() {
+        Ctx.setRequest("name", "limingwei");
+        Ctx.setRequest("date", new Date());
+        Ctx.freemarker("WEB-INF/view_fm/fm.htm");
+    }
+
+    @Test
+    public void velocity() {
+        Ctx.setRequest("name", "limingwei");
+        Ctx.setRequest("date", new Date());
+        Ctx.velocity("WEB-INF/view_vl/vl.htm");
+    }
+
+    @Test
+    public void beetl() {
+        Ctx.setRequest("name", "limingwei");
+        Ctx.setRequest("date", new Date());
+        Ctx.beetl("WEB-INF/view_bt/bt.htm");
     }
 }
