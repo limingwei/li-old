7014eb240ca9d0eb66d857b46e0e6c421db8557a
li <li@li-pc>
2013-06-20 13:51:52 +0800

    代码整理

--- test/li/MoreTest.java
+++ test/li/MoreTest.java
@@ -5,7 +5,6 @@ import java.io.File;
 import li.aop._Account;
 import li.ioc.Ioc;
 import li.lang.Code;
-import li.lang.Code.CodeAnalysisConf;
 import net.sf.cglib.core.DebuggingClassWriter;
 
 import org.junit.Test;
@@ -13,19 +12,11 @@ import org.junit.Test;
 public class MoreTest {
     @Test
     public void codeAnalysis() {
-        String suffix = "java";
-        boolean countSubFolder = true;
-        CodeAnalysisConf conf = null;
-
-        System.out.println(Code.countingCode(new File("framework"), suffix, countSubFolder, conf));
-        System.out.println();
-        System.out.println(Code.countingCode(new File("more"), suffix, countSubFolder, conf));
-        System.out.println();
-        System.out.println(Code.countingCode(new File("dev"), suffix, countSubFolder, conf));
-        System.out.println();
-        System.out.println(Code.countingCode(new File("test"), suffix, countSubFolder, conf));
-        System.out.println();
-        System.out.println(Code.countingCode(new File("demo"), suffix, countSubFolder, conf));
+        System.out.println(Code.countingCode(new File("framework"), "java", true, null) + "\n");
+        System.out.println(Code.countingCode(new File("more"), "java", true, null) + "\n");
+        System.out.println(Code.countingCode(new File("dev"), "java", true, null) + "\n");
+        System.out.println(Code.countingCode(new File("test"), "java", true, null) + "\n");
+        System.out.println(Code.countingCode(new File("demo"), "java", true, null) + "\n");
     }
 
     @Test
@@ -38,15 +29,27 @@ public class MoreTest {
     public void multiThreadDao() throws InterruptedException {
         final _Account accountDao = Ioc.get(_Account.class);
 
-        for (int i = 0; i < 1000; i++) {// 开启1000个线程
+        final int threadNum = 100, execNum = 100;
+        long threadSleep = 50, stop = 0, start = System.currentTimeMillis();
+
+        for (int i = 0; i < threadNum; i++) {
             new Thread() {
                 public void run() {
-                    for (; true;) {// 每个线程无限循环
-                        accountDao.update("SET status=1 WHERE id=11");// 这是一个带Aop带Trans的Dao方法
+                    for (int i = 0; i < execNum; i++) {
+                        accountDao.update("SET flag=1 WHERE id=11");// 这是一个带Aop带Trans的Dao方法
                     }
                 }
             }.start();
-            Thread.sleep(1000);// 每隔1秒增加一个线程
+            Thread.sleep(threadSleep);
+            stop += threadSleep;
         }
+
+        long now = System.currentTimeMillis();
+        System.out.println("线程总数\t\t" + threadNum);
+        System.out.println("单线程操作数\t" + execNum);
+        System.out.println("操作总数\t\t" + (threadNum * execNum));
+        System.out.println("总计耗时(毫秒)\t" + (now - start));
+        System.out.println("线程中断(毫秒)\t" + stop);
+        System.out.println("实际耗时(毫秒)\t" + (now - start - stop));
     }
 }
