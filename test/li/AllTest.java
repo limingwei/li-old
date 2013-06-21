31bb072f2183a4940f85898e532f3bf80f0951f6
li <li@li-pc>
2013-06-21 13:35:28 +0800

    测试代码整理

--- test/li/AllTest.java
+++ test/li/AllTest.java
@@ -1,5 +1,13 @@
 package li;
 
+import li.aop.AopTest;
+import li.dao.AbstractDaoTest;
+import li.dao.ModelBuilderTest;
+import li.dao.PageTest;
+import li.dao.QueryBuilderTest;
+import li.dao.QueryRunnerTest;
+import li.dao.RecordTest;
+import li.dao.TransTest;
 import li.demo.action.AccountActionTest;
 import li.demo.action.DemoActionTest;
 import li.demo.action.PostActionTest;
@@ -7,16 +15,32 @@ import li.demo.record.AccountTest;
 import li.demo.record.ForumTest;
 import li.demo.record.PostTest;
 import li.demo.record.RoleTest;
+import li.ioc.IocTest;
+import li.model.BeanTest;
+import li.model.FieldTest;
+import li.mvc.ContextTest;
+import li.service.AbstractServiceTest;
+import li.util.ConvertTest;
+import li.util.FilesTest;
+import li.util.LogTest;
+import li.util.ReflectTest;
+import li.util.VerifyTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ FrameworkTest.class, //
-        AccountTest.class, PostTest.class, ForumTest.class, RoleTest.class,//
-        DemoActionTest.class, AccountActionTest.class,//
-        PostActionTest.class })
+@SuiteClasses({ ContextTest.class,// MVC
+        AbstractServiceTest.class, // Service
+        IocTest.class,// Ioc
+        AopTest.class, // Aop
+        BeanTest.class, FieldTest.class, // Model
+        AbstractDaoTest.class, QueryRunnerTest.class, RecordTest.class, PageTest.class, QueryBuilderTest.class, ModelBuilderTest.class, TransTest.class,// Dao
+        ConvertTest.class, ReflectTest.class, FilesTest.class, LogTest.class, VerifyTest.class, // Util
+        AccountTest.class, PostTest.class, ForumTest.class, RoleTest.class,// Record
+        DemoActionTest.class, AccountActionTest.class, PostActionTest.class // Action
+})
 public class AllTest {
 
 }
