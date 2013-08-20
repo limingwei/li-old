478470fba935cf8d4e8d9a2696e12aecc62d7acd
明伟 <@li-pc>
2013-08-21 00:16:04 +0800

    httl Methods

--- more/li/mvc/view/HttlView.java
+++ more/li/mvc/view/HttlView.java
@@ -41,27 +41,3 @@ public class HttlView extends AbstractView {
         log.info("httl to : ?", tempPath);
     }
 }
-
-class HttlMethods {
-    public static String left(Object self, int len) {
-        return left(self + "", len);
-    }
-
-    public static String left(String text, int len) {
-        if (text.length() <= len) {
-            return text;
-        }
-        return text.substring(0, len);
-    }
-
-    public static String right(Object self, int len) {
-        return right(self + "", len);
-    }
-
-    public static String right(String text, int len) {
-        if (text.length() <= len) {
-            return text;
-        }
-        return text.substring(text.length() - len, text.length());
-    }
-}
