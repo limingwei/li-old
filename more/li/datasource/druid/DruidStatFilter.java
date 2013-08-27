a0d41fb22c440fef7771bed70a3adc10b3a91fd6
li.co <limingwei@mail.com>
2013-08-27 09:50:44 +0800

    DruidStatFilter实现

--- more/li/datasource/druid/DruidStatFilter.java
+++ more/li/datasource/druid/DruidStatFilter.java
@@ -18,37 +18,27 @@ import com.alibaba.druid.support.spring.stat.SpringStatManager;
  * @author 明伟
  */
 public class DruidStatFilter implements AopFilter {
-    public static final String PROP_NAME_PORFILE = "druid.profile";
     private static SpringStat springStat = new SpringStat();
 
     private SpringMethodContextListener statContextlistener = new SpringMethodContextListener();
 
-    public void afterPropertiesSet() throws Exception {
-        SpringStatManager.getInstance().addSpringStat(springStat);
+    public static final String PROP_NAME_PORFILE = "druid.profile";
 
+    public DruidStatFilter() throws Exception {
+        SpringStatManager.getInstance().addSpringStat(springStat);
         StatFilterContext.getInstance().addContextListener(this.statContextlistener);
     }
 
-    public void destroy() throws Exception {
-        StatFilterContext.getInstance().removeContextListener(this.statContextlistener);
-    }
-
     public void doFilter(AopChain chain) {
-        System.err.println("DruidStatFilter.doFilter() --> " + chain.getTarget().getClass() + "." + chain.getMethod().getName() + "()");
         SpringMethodStat lastMethodStat = SpringMethodStat.current();
-
         SpringMethodInfo methodInfo = getMethodInfo(chain);
-
         SpringMethodStat methodStat = springStat.getMethodStat(methodInfo, true);
-
         if (methodStat != null) {
-            System.err.println("methodStat.beforeInvoke();");
             methodStat.beforeInvoke();
         }
         long startNanos = System.nanoTime();
         Throwable error = null;
         try {
-            System.err.println("chain.doFilter();");
             chain.doFilter();
         } catch (Throwable e) {
             throw new RuntimeException(e + " ", e);
@@ -56,7 +46,6 @@ public class DruidStatFilter implements AopFilter {
             long endNanos = System.nanoTime();
             long nanos = endNanos - startNanos;
             if (methodStat != null) {
-                System.err.println("methodStat.afterInvoke(error, nanos);");
                 methodStat.afterInvoke(error, nanos);
             }
             SpringMethodStat.setCurrent(lastMethodStat);
@@ -68,40 +57,43 @@ public class DruidStatFilter implements AopFilter {
         return new SpringMethodInfo(method.getDeclaringClass(), method);
     }
 
-    class SpringMethodContextListener extends StatFilterContextListenerAdapter {
-        SpringMethodContextListener() {}
-
+    private class SpringMethodContextListener extends StatFilterContextListenerAdapter {
         public void addUpdateCount(int updateCount) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.addJdbcUpdateCount(updateCount);
+            }
         }
 
         public void addFetchRowCount(int fetchRowCount) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.addJdbcFetchRowCount(fetchRowCount);
+            }
         }
 
         public void executeBefore(String sql, boolean inTransaction) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.incrementJdbcExecuteCount();
+            }
         }
 
         public void executeAfter(String sql, long nanos, Throwable error) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
             if (springMethodStat != null) {
                 springMethodStat.addJdbcExecuteTimeNano(nanos);
-                if (error != null)
+                if (error != null) {
                     springMethodStat.incrementJdbcExecuteErrorCount();
+                }
             }
         }
 
         public void commit() {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.incrementJdbcCommitCount();
+            }
         }
 
         public void rollback() {
@@ -112,26 +104,30 @@ public class DruidStatFilter implements AopFilter {
 
         public void pool_connect() {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.incrementJdbcPoolConnectionOpenCount();
+            }
         }
 
         public void pool_close(long nanos) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.incrementJdbcPoolConnectionCloseCount();
+            }
         }
 
         public void resultSet_open() {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.incrementJdbcResultSetOpenCount();
+            }
         }
 
         public void resultSet_close(long nanos) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null)
+            if (springMethodStat != null) {
                 springMethodStat.incrementJdbcResultSetCloseCount();
+            }
         }
     }
 }
