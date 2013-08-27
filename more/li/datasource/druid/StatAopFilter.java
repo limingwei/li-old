dc10b79b20368feb61348425dc786f444c31d9a6
li.co <limingwei@mail.com>
2013-08-27 15:23:40 +0800

    StatAopFilter

--- more/li/datasource/druid/StatAopFilter.java
+++ more/li/datasource/druid/StatAopFilter.java
@@ -20,12 +20,13 @@ public class StatAopFilter implements AopFilter {
 
     public StatAopFilter() throws Exception {
         SpringStatManager.getInstance().addSpringStat(springStat);
-        StatFilterContext.getInstance().addContextListener(new SpringMethodContextListener());
+        StatFilterContext.getInstance().addContextListener(new AopMethodContextListener());
     }
 
     public void doFilter(AopChain chain) {
         SpringMethodStat lastMethodStat = SpringMethodStat.current();
-        SpringMethodStat methodStat = springStat.getMethodStat(new SpringMethodInfo(chain.getMethod().getDeclaringClass(), chain.getMethod()), true);
+        SpringMethodInfo methodInfo = new SpringMethodInfo(chain.getMethod().getDeclaringClass()/* chain.getTarget().getClass() */, chain.getMethod());
+        SpringMethodStat methodStat = springStat.getMethodStat(methodInfo, true);
         if (methodStat != null) {
             methodStat.beforeInvoke();
         }
@@ -36,51 +37,48 @@ public class StatAopFilter implements AopFilter {
         } catch (Throwable e) {
             throw new RuntimeException(e + " ", error = e);
         } finally {
-            long nanos = System.nanoTime() - startNanos;
             if (methodStat != null) {
-                methodStat.afterInvoke(error, nanos);
+                methodStat.afterInvoke(error, System.nanoTime() - startNanos);
             }
             SpringMethodStat.setCurrent(lastMethodStat);
         }
     }
 
-    private class SpringMethodContextListener extends StatFilterContextListenerAdapter {
+    /**
+     * AopMethodContextListener
+     */
+    private class AopMethodContextListener extends StatFilterContextListenerAdapter {
         public void addUpdateCount(int updateCount) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.addJdbcUpdateCount(updateCount);
-            }
         }
 
         public void addFetchRowCount(int fetchRowCount) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.addJdbcFetchRowCount(fetchRowCount);
-            }
         }
 
         public void executeBefore(String sql, boolean inTransaction) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.incrementJdbcExecuteCount();
-            }
         }
 
         public void executeAfter(String sql, long nanos, Throwable error) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
             if (springMethodStat != null) {
                 springMethodStat.addJdbcExecuteTimeNano(nanos);
-                if (error != null) {
+                if (error != null)
                     springMethodStat.incrementJdbcExecuteErrorCount();
-                }
             }
         }
 
         public void commit() {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.incrementJdbcCommitCount();
-            }
         }
 
         public void rollback() {
@@ -91,30 +89,26 @@ public class StatAopFilter implements AopFilter {
 
         public void pool_connect() {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.incrementJdbcPoolConnectionOpenCount();
-            }
         }
 
         public void pool_close(long nanos) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.incrementJdbcPoolConnectionCloseCount();
-            }
         }
 
         public void resultSet_open() {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.incrementJdbcResultSetOpenCount();
-            }
         }
 
         public void resultSet_close(long nanos) {
             SpringMethodStat springMethodStat = SpringMethodStat.current();
-            if (springMethodStat != null) {
+            if (springMethodStat != null)
                 springMethodStat.incrementJdbcResultSetCloseCount();
-            }
         }
     }
 }
