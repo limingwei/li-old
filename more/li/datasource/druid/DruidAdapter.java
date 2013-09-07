358ef73a1c5ea13d3bf7c78638faebfa0cce9cba
明伟 <@li-pc>
2013-09-07 12:02:12 +0800

    注释和log更新

--- more/li/datasource/druid/DruidAdapter.java
+++ more/li/datasource/druid/DruidAdapter.java
@@ -4,12 +4,15 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import li.ioc.Ioc;
+import li.util.Log;
 import li.util.Verify;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
 public class DruidAdapter extends DruidDataSource {
     private static final long serialVersionUID = 8840084696561752691L;
+
+    private static final Log log = Log.init();
 
     /**
      * 替换内置的setFilters
@@ -18,18 +21,20 @@ public class DruidAdapter extends DruidDataSource {
         String[] filterArray = (Verify.isEmpty(filters)) ? new String[0] : filters.split("\\,");
         for (String item : filterArray) {
             if ("wall".equals(item)) {
+                log.info("using li.datasource.druid.WallDruidFilter");
                 WallDruidFilter wallFilter = Ioc.get(WallDruidFilter.class);
-                if (null == wallFilter) {
-                    wallFilter = new WallDruidFilter();
-                }
-                super.getProxyFilters().add(wallFilter);
+                super.getProxyFilters().add(null != wallFilter ? wallFilter : new WallDruidFilter());
             } else {
                 super.setFilters(item);
             }
         }
     }
 
+    /**
+     * 不允许初始化DataSource后使用不同的密码获取Connection
+     */
     public Connection getConnection(String username, String password) throws SQLException {
+        log.warn("not allowed to get a connection using new username and password after datasource inited !!!");
         return super.getConnection();
     }
 }
